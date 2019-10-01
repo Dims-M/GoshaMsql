7029a6e7ee55360148e4c501f7e999044f098636
Дмитрий <rabolan@mail.ru>
2019-10-01 17:47:31 +0300

    допилили авторизацию

--- RegisterForm.Designer.cs
+++ RegisterForm.Designer.cs
@@ -34,9 +34,13 @@
             this.SaveRegButton = new MaterialSkin.Controls.MaterialFlatButton();
             this.ExitRegButton = new MaterialSkin.Controls.MaterialFlatButton();
             this.textBoxUser = new System.Windows.Forms.TextBox();
-            this.passUserFild = new System.Windows.Forms.TextBox();
             this.userRegEmail = new System.Windows.Forms.TextBox();
             this.clearFildGerForm = new MaterialSkin.Controls.MaterialFlatButton();
+            this.passUserFild = new System.Windows.Forms.TextBox();
+            this.NamTtextBox = new System.Windows.Forms.TextBox();
+            this.nameRegFormLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.ComentReTtextBox = new System.Windows.Forms.TextBox();
+            this.materialLabel4 = new MaterialSkin.Controls.MaterialLabel();
             this.SuspendLayout();
             // 
             // materialLabel1
@@ -48,9 +52,9 @@
             this.materialLabel1.Location = new System.Drawing.Point(12, 96);
             this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel1.Name = "materialLabel1";
-            this.materialLabel1.Size = new System.Drawing.Size(53, 24);
+            this.materialLabel1.Size = new System.Drawing.Size(64, 24);
             this.materialLabel1.TabIndex = 1;
-            this.materialLabel1.Text = "ИМЯ";
+            this.materialLabel1.Text = "Логин";
             // 
             // materialLabel2
             // 
@@ -58,7 +62,7 @@
             this.materialLabel2.Depth = 0;
             this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
             this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel2.Location = new System.Drawing.Point(12, 158);
+            this.materialLabel2.Location = new System.Drawing.Point(12, 178);
             this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel2.Name = "materialLabel2";
             this.materialLabel2.Size = new System.Drawing.Size(86, 24);
@@ -71,7 +75,7 @@
             this.materialLabel3.Depth = 0;
             this.materialLabel3.Font = new System.Drawing.Font("Roboto", 11F);
             this.materialLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel3.Location = new System.Drawing.Point(12, 216);
+            this.materialLabel3.Location = new System.Drawing.Point(12, 222);
             this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel3.Name = "materialLabel3";
             this.materialLabel3.Size = new System.Drawing.Size(82, 24);
@@ -120,19 +124,9 @@
             this.textBoxUser.Enter += new System.EventHandler(this.TextBoxUser_Enter);
             this.textBoxUser.Leave += new System.EventHandler(this.TextBoxUser_Leave);
             // 
-            // passUserFild
-            // 
-            this.passUserFild.Location = new System.Drawing.Point(239, 158);
-            this.passUserFild.Name = "passUserFild";
-            this.passUserFild.PasswordChar = '*';
-            this.passUserFild.Size = new System.Drawing.Size(168, 22);
-            this.passUserFild.TabIndex = 7;
-            this.passUserFild.Enter += new System.EventHandler(this.PassUserFild_Enter);
-            this.passUserFild.Leave += new System.EventHandler(this.PassUserFild_Leave);
-            // 
             // userRegEmail
             // 
-            this.userRegEmail.Location = new System.Drawing.Point(239, 218);
+            this.userRegEmail.Location = new System.Drawing.Point(239, 224);
             this.userRegEmail.Name = "userRegEmail";
             this.userRegEmail.Size = new System.Drawing.Size(168, 22);
             this.userRegEmail.TabIndex = 8;
@@ -145,7 +139,7 @@
             this.clearFildGerForm.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.clearFildGerForm.Depth = 0;
             this.clearFildGerForm.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.clearFildGerForm.Location = new System.Drawing.Point(239, 263);
+            this.clearFildGerForm.Location = new System.Drawing.Point(247, 309);
             this.clearFildGerForm.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.clearFildGerForm.MouseState = MaterialSkin.MouseState.HOVER;
             this.clearFildGerForm.Name = "clearFildGerForm";
@@ -156,11 +150,66 @@
             this.clearFildGerForm.UseVisualStyleBackColor = true;
             this.clearFildGerForm.Click += new System.EventHandler(this.ClearFildGerForm_Click);
             // 
+            // passUserFild
+            // 
+            this.passUserFild.Location = new System.Drawing.Point(239, 178);
+            this.passUserFild.Name = "passUserFild";
+            this.passUserFild.PasswordChar = '*';
+            this.passUserFild.Size = new System.Drawing.Size(168, 22);
+            this.passUserFild.TabIndex = 7;
+            this.passUserFild.Enter += new System.EventHandler(this.PassUserFild_Enter);
+            this.passUserFild.Leave += new System.EventHandler(this.PassUserFild_Leave);
+            // 
+            // NamTtextBox
+            // 
+            this.NamTtextBox.Location = new System.Drawing.Point(239, 137);
+            this.NamTtextBox.Name = "NamTtextBox";
+            this.NamTtextBox.Size = new System.Drawing.Size(168, 22);
+            this.NamTtextBox.TabIndex = 10;
+            this.NamTtextBox.Enter += new System.EventHandler(this.NamTtextBox_Enter);
+            // 
+            // nameRegFormLabel
+            // 
+            this.nameRegFormLabel.AutoSize = true;
+            this.nameRegFormLabel.Depth = 0;
+            this.nameRegFormLabel.Font = new System.Drawing.Font("Roboto", 11F);
+            this.nameRegFormLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.nameRegFormLabel.Location = new System.Drawing.Point(12, 135);
+            this.nameRegFormLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.nameRegFormLabel.Name = "nameRegFormLabel";
+            this.nameRegFormLabel.Size = new System.Drawing.Size(53, 24);
+            this.nameRegFormLabel.TabIndex = 11;
+            this.nameRegFormLabel.Text = "ИМЯ";
+            // 
+            // ComentReTtextBox
+            // 
+            this.ComentReTtextBox.Location = new System.Drawing.Point(239, 261);
+            this.ComentReTtextBox.Name = "ComentReTtextBox";
+            this.ComentReTtextBox.Size = new System.Drawing.Size(168, 22);
+            this.ComentReTtextBox.TabIndex = 12;
+            // 
+            // materialLabel4
+            // 
+            this.materialLabel4.AutoSize = true;
+            this.materialLabel4.Depth = 0;
+            this.materialLabel4.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel4.Location = new System.Drawing.Point(12, 261);
+            this.materialLabel4.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel4.Name = "materialLabel4";
+            this.materialLabel4.Size = new System.Drawing.Size(120, 24);
+            this.materialLabel4.TabIndex = 13;
+            this.materialLabel4.Text = "Коментарий";
+            // 
             // RegisterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(470, 433);
+            this.Controls.Add(this.materialLabel4);
+            this.Controls.Add(this.ComentReTtextBox);
+            this.Controls.Add(this.nameRegFormLabel);
+            this.Controls.Add(this.NamTtextBox);
             this.Controls.Add(this.clearFildGerForm);
             this.Controls.Add(this.userRegEmail);
             this.Controls.Add(this.passUserFild);
@@ -187,8 +236,12 @@
         private MaterialSkin.Controls.MaterialFlatButton SaveRegButton;
         private MaterialSkin.Controls.MaterialFlatButton ExitRegButton;
         public System.Windows.Forms.TextBox textBoxUser;
-        private System.Windows.Forms.TextBox passUserFild;
         private System.Windows.Forms.TextBox userRegEmail;
         private MaterialSkin.Controls.MaterialFlatButton clearFildGerForm;
+        private System.Windows.Forms.TextBox passUserFild;
+        private System.Windows.Forms.TextBox NamTtextBox;
+        private MaterialSkin.Controls.MaterialLabel nameRegFormLabel;
+        private System.Windows.Forms.TextBox ComentReTtextBox;
+        private MaterialSkin.Controls.MaterialLabel materialLabel4;
     }
 }
--- RegisterForm.cs
+++ RegisterForm.cs
@@ -8,20 +8,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace GoshaMsql
 {
     public partial class RegisterForm : MaterialForm
     {
+        private DB db;
+
         public RegisterForm()
         {
             InitializeComponent();
-            textBoxUser.Text = "Введите имя";
+
+            textBoxUser.Text = "Введите логин";
             passUserFild.Text = "Пароль";
             userRegEmail.Text = "Введите почту";
+            NamTtextBox.Text = "Введите Имя";
+            ComentReTtextBox.Text = "При необходимости коментарий";
+
             textBoxUser.ForeColor = Color.Gray;
             passUserFild.ForeColor = Color.Gray;
             userRegEmail.ForeColor = Color.Gray;
+            NamTtextBox.ForeColor = Color.Gray;
+            ComentReTtextBox.ForeColor = Color.Gray;
         }
 
         //форма
@@ -33,7 +42,46 @@ namespace GoshaMsql
         //сохранить введенные даннын
         private void SaveRegButton_Click(object sender, EventArgs e)
         {
+            string log = "";
 
+           // try
+          //  {
+
+             
+            db = new DB(); //контекст для работы с БД
+            //MySqlCommand command = new MySqlCommand("INSERT INTO 'users' (`id`,'login', 'pass', 'name','email', 'coment', 'AddDateTime',`DateTimeExit`) VALUES(NULL,@login, @pass, @name, @email, @coment, '' )", db.GetConnection()); // для работы с запросами БД
+            MySqlCommand command = new MySqlCommand($"INSERT INTO `users` (`login`, `pass`, `name`,`email`, `coment`, `AddDateTime`,`DateTimeExit`) VALUES('{textBoxUser.Text}', '{passUserFild.Text}', {NamTtextBox.Text}, '{userRegEmail.Text}','{ComentReTtextBox.Text}','','')", db.GetConnection()); // для работы с запросами БД
+         // MySqlCommand command = new MySqlCommand("INSERT INTO `users` (`id`, `login`, `pass`, `name`, `email`, `coment`, `AddDateTime`, `DateTimeExit`) VALUES(NULL, 'пробный', '12345', 'тест', 'fgeg@.mail.ru', 'тестовой', '2019-10-01 00:00:00', '')",db.GetConnection());
+
+            //параметры скл запроса
+            //command.Parameters.Add("@login",MySqlDbType.VarChar).Value = textBoxUser.Text; // присваеваем значение из тек поля вводаа логина
+            //command.Parameters.Add("@pass", MySqlDbType.VarChar).Value = passUserFild.Text;
+            //command.Parameters.Add("@email", MySqlDbType.VarChar).Value = userRegEmail.Text;
+            //command.Parameters.Add("@name", MySqlDbType.VarChar).Value = NamTtextBox.Text;
+            //command.Parameters.Add("@coment", MySqlDbType.VarChar).Value = ComentReTtextBox.Text;
+            //command.Parameters.Add("@AddDateTime", MySqlDbType.DateTime).Value = DateTime.Now;
+
+            db.openConnection(); // открытие соединения
+
+            if (command.ExecuteNonQuery() == 1) // выполняет результат запроса скл. С возратом значения. Если 1 то успешно
+            {
+                MessageBox.Show("Пользователь добавлен.");
+                log += $"{DateTime.Now.ToString()} - Пользователь добавлен! {NamTtextBox.Text}\t\n";
+                BL.WrateText(log);
+            }
+
+            else 
+                MessageBox.Show("Ошибка при добавлении пользователя");
+
+           // }
+
+            //catch (Exception ex)
+            //{
+            //    log += $"ОШИБКА при добавлении пользователя!! \t\n{ex}";
+            //    BL.WrateText(log);
+            //}
+        
+            db.closeConnection();// закрытие соединения
         }
 
         /// <summary>
@@ -91,7 +139,7 @@ namespace GoshaMsql
              
         }
 
-        //Обработчик события. Сработывает при выходе из формы вв ода
+        //Обработчик события. Сработывает при выходе из формы ввода имени
         private void TextBoxUser_Leave(object sender, EventArgs e)
         {
             if (textBoxUser.Text == "")
@@ -111,11 +159,22 @@ namespace GoshaMsql
             }
         }
 
+        //Обработчик события.Сработывает при выходе из формы при вводе почты регистрации.
         private void UserRegEmail_Leave(object sender, EventArgs e)
         {
             if (userRegEmail.Text == "")
             {
                 userRegEmail.Text = "Введите почту";
+                userRegEmail.ForeColor = Color.Red;
+            }
+        }
+
+        ////Обработчик события.Сработывает при выходе из формы при вводе имени
+        private void NamTtextBox_Enter(object sender, EventArgs e)
+        {
+            if (userRegEmail.Text == "")
+            {
+                userRegEmail.Text = "Введите Имя";
                 userRegEmail.ForeColor = Color.Red;
             }
         }
